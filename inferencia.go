e7a6fe21c544ce72bf7909bb0a04ecc825ab7b98
Jeferson Luis <jgjefersonluis@gmail.com>
2022-06-16 01:46:52 -0300

    inferencia

---
+++ inferencia.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	//	var i int
+	//var j = i //int
+
+	//fmt.Printf("Tipo: %T Valor: %v\n", j, j)
+
+	var i = 36.87
+	fmt.Printf("Tipo: %T Valor: %v\n", i, i)
+
+}
