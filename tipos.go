fed8b966e54e9815a6b8ca3067b799eefa662f68
Jeferson Luis <jgjefersonluis@gmail.com>
2022-06-16 01:27:26 -0300

    tipos

---
+++ tipos.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var altura = 1.34
+	var aberto bool = true
+
+	fmt.Printf("Tipo: %T Valor: %v\n", altura, altura)
+	fmt.Printf("Tipo: %T Valor: %v\n", aberto, aberto)
+}
