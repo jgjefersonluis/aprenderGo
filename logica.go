d5d5b7296deaa374bfbb5f581e90a9e892006135
Jeferson Luis <jgjefersonluis@gmail.com>
2022-06-19 22:33:33 -0300

    logica

---
+++ logica.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// && || !
+	// 3 < x < 7
+	// x > 3 && x < 7
+
+	var x int = 9
+	fmt.Println("x > 3 && x < 7", x > 3 && x < 7)
+
+	fmt.Println("x < 3 || x > 7", x < 3 || x > 7)
+
+	fmt.Println(!true)
+}
