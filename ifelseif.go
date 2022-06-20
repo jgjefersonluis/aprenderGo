b46131af9540416f3a4d73f1f174fa4d66b0ac3b
Jeferson Luis <jgjefersonluis@gmail.com>
2022-06-19 22:47:11 -0300

    ifelseif

---
+++ ifelseif.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// if else else if
+
+	//var a int = 11
+
+	// a > 10: "a é maior que 10"
+	// a <= 10 && a > 5: "a está entre 6 e 10"
+	// a <= 5 "a é menor ou igual a 10"
+
+	if a := 2; a > 10 {
+		fmt.Println("a é maior que 10")
+	} else if a > 5 {
+		fmt.Println("a está entre 6 e 10")
+	} else {
+		fmt.Println("a é menor ou igual a 10")
+	}
+}
