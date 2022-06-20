827bb46cb1e1bb8a6b0bd5d10bd87ddd8049446a
Jeferson Luis <jgjefersonluis@gmail.com>
2022-06-19 21:43:53 -0300

    operador aritmetico

---
+++ aritmetica.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// operador matematico + - * / %
+
+	var a float64 = 23
+	var b float64 = 7
+
+	var c int = 23
+	var d int = 7
+
+	fmt.Println("a + b =", float64(a)+b)
+	//fmt.Println("a - b =", float64(a) - b)
+	//fmt.Println("a * b =", float64(a) * b)
+	//fmt.Println("a / b =", float64(a) / b)
+	fmt.Println("c % d =", c%d)
+
+}
